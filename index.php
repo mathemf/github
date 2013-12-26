6404ee616302bfe951b269c427a3a0978d0b23f8
gutoaa <augusto@augustoandrade.com>
2013-12-26 17:24:37 -0200

    Commit teste #1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <?php
-            echo 'estou mudando denovo e denovo';
+            echo 'estou mudando denovo e denovo E DENOVO E DENOVO E DENOVO #1';
         ?>
     </body>
 </html>
