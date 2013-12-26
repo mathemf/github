9beb5b04b12d8cb6eb6f344f4dd99d91e53a0b61
mathemf <apple@192.168.3.100>
2013-12-26 17:43:10 -0200

    commit 5

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <?php
-            echo 'estou mudando denovo e denovo E DENOVO E DENOVO E DENOVO #1';
+            echo 'Estou mudando - DENOVO';
         ?>
     </body>
 </html>
